b2646ce62b755aff94af42e668a706343063f4a9
Tiferod <dorian.paret@ecl14.ec-lyon.fr>
2018-01-23 19:15:37 +0100

    Rajout de la disponibilité des documents sur leur page dédiée

--- document.php
+++ document.php
@@ -15,41 +15,56 @@
 		<link href="style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
+		<?php
+			include('header.php');
+		?>
+		<h1><?php echo $doc[1]; ?></h1>
 		<p>
 			<?php
-				include('header.php');
+			switch ($doc[2]) {
+				case "livre":
+					$auteur = mysqli_fetch_row($resultAuteur);
+					echo 'Paru le ' . date("d/m/Y", strtotime($doc[6])) . ' et édité par ' . $doc[3] . ', ' . $doc[1] . ' est un livre écrit par ' . $auteur[0] . '.';
+				break;
+				case "BD":
+					echo 'Parue le ' . date("d/m/Y", strtotime($doc[6])) . ' et éditée par ' . $doc[3] . ', ' . $doc[1] . ' est une BD réalisée par ';
+					while ($auteur = mysqli_fetch_row($resultAuteur)) {
+					 	echo $auteur[0];
+					 	if ($nbrAuteur > 2) {echo ', ';}
+					 	if ($nbrAuteur  == 2) {echo ' et ';}
+					 	$nbrAuteur--;
+					 }
+					echo '. Elle est la ' . $doc[5];
+					if ($doc[5] == 1) {echo 'ère';} else {echo 'ème';}
+					echo ' de la série ' . $doc[4] . '.';
+				break;
+				default:
+					echo 'Parue le ' . date("d/m/Y", strtotime($doc[6])) . ' et éditée par ' . $doc[3] . ', ' . $doc[1] . ' est une oeuvre écrite par ';
+					for ($i=$nbrAuteur-1; $i >= 0 ; $i--) { 
+					 	echo $auteur[$i];
+					 	if ($i > 1) {echo ', ';}
+					 	if ($i == 1) {echo ' et ';}
+					 	if ($i == 0) {echo '.';}
+					 }
+				break;
+			}
 			?>
-			<h1><?php echo $doc[1]; ?></h1>
-			<p>
-				<?php
-				switch ($doc[2]) {
-					case "livre":
-						$auteur = mysqli_fetch_row($resultAuteur);
-						echo 'Paru le ' . date("d/m/Y", strtotime($doc[6])) . ' et édité par ' . $doc[3] . ', ' . $doc[1] . ' est un livre écrit par ' . $auteur[0] . '.';
-					break;
-					case "BD":
-						echo 'Parue le ' . date("d/m/Y", strtotime($doc[6])) . ' et éditée par ' . $doc[3] . ', ' . $doc[1] . ' est une BD réalisée par ';
-						while ($auteur = mysqli_fetch_row($resultAuteur)) {
-						 	echo $auteur[0];
-						 	if ($nbrAuteur > 2) {echo ', ';}
-						 	if ($nbrAuteur  == 2) {echo ' et ';}
-						 	$nbrAuteur--;
-						 }
-						echo '. Elle est la ' . $doc[5];
-						if ($doc[5] == 1) {echo 'ère';} else {echo 'ème';}
-						echo ' de la série ' . $doc[4] . '.';
-					break;
-					default:
-						echo 'Parue le ' . date("d/m/Y", strtotime($doc[6])) . ' et éditée par ' . $doc[3] . ', ' . $doc[1] . ' est une oeuvre écrite par ';
-						for ($i=$nbrAuteur-1; $i >= 0 ; $i--) { 
-						 	echo $auteur[$i];
-						 	if ($i > 1) {echo ', ';}
-						 	if ($i == 1) {echo ' et ';}
-						 	if ($i == 0) {echo '.';}
-						 }
-					break;
+		</p>
+		<p>
+			</br>
+			</br>
+			<?php
+				if($doc[7])
+				{
+					echo 'Ce document est actuellement disponible à l\'emprunt.';
 				}
-				?>
+				else
+				{
+					$resultDate = mysqli_query($db, "SELECT date_retour_prevue FROM Emprunts WHERE document = '$doc_id'");
+					$date = mysqli_fetch_row($resultDate);
+					echo 'Ce document n\'est actuellement pas disponible à l\'emprunt. Il devrait être retourné avant le ' . date("d/m/Y", strtotime($date[0])) . '.';
+				}
+			?>
 		</p>
 	</body>
 </html>
